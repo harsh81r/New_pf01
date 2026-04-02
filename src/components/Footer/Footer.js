a0cfeb795b33c0af5c1c5e9d068b7a5fe0cb4583
harsh khare <153971247+harsh81r@users.noreply.github.com>
2026-04-02 09:51:45 +0530

    Remove commented-out logo from Footer componentremove logo
    Removed commented-out logo Link component from Footer.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,13 +6,7 @@ function Footer() {
             <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
                 <div className="md:flex md:justify-between">
                     <div className="mb-6 md:mb-0">
-                        <Link to="/" className="flex items-center">
-                            // <img
-                            //     src="https://media.licdn.com/dms/image/v2/D5603AQF-RFNeGK7BDw/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1727793359643?e=1733356800&v=beta&t=yODdgzG3sXpYaC2JQftc_v4E72CdWE5XyOBtz99v-xA"
-                            //     className="mr-6 h-13 rounded-full"
-                            //     alt="Logo"
-                            // />
-                        </Link>
+                    
                     </div>
                     <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                         <div>
